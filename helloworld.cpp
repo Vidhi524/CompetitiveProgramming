2bb532c495ff98809d05c37d890761f40d144408
Vidhi524 <vidhi.arora@logicladder.com>
2025-07-11 00:29:50 +0530

    4

---
+++ helloworld.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    //pair
+    // pair<int,int> p;
+    // pair<int, string> p2;
+    // p= make_pair(1,2); // bracket used =()
+    // p2={1,"Vidhi"}; // same a make_pair barcket used = {}
+    // cout <<p.first<<" "<<p.second<<endl;
+    // cout <<p2.first<<" "<<p2.second<<endl;
+    // int a[] = {1,2,3};
+    // int b[] = {2,3,4}; // a[] and b[] are related => store in pair od array.
+
+
+    //making array of pair
+    // pair<int,int> array_of_pair[3];
+    // array_of_pair[0]= {1,2};
+    // array_of_pair[1]= {2,3};
+    // array_of_pair[2]= {3,4};
+
+    // swaping pair as a whole
+
+    // swap(array_of_pair[0],array_of_pair[2]);
+
+    //printing array of pairs
+
+    // for(int i=0;i<3;i++){
+    //     cout<<array_of_pair[i].first<<" "<<array_of_pair[i].second<<endl;
+    // }
+
+
+    //taking input
+    pair<int,int> p;
+    cout<<"enter 1: ";
+    cin>>p.first;
+    cout<<"enter 2: ";
+    cin>>p.second;
+    
+
+    return 0;
+}
